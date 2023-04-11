8746065e5f0b9ee358b2909f0ac897ef7b47ae78
zabokaa <>
2023-04-11 18:22:50 +0200

    Created script.js with first jQuery code

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+$('input').addClass('red');
+
+// Below, only make the .main-button green
+$(".main-button").addClass("green")
+// Below, make the text of all buttons yellow
+$("button").addClass("text--yellow")
