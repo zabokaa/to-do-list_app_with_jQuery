552a2c850464b320d661d38467b243f8a1939922
zabokaa <>
2023-04-11 20:58:19 +0200

    Added newItem func

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
 // USING jQUERY ONLY
 
-
 // [1] user can add new item to the list
+function newItem() {
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    let text = $("<span></span>").text(inputValue);
+    li.append(text);
 
+    if (inputValue === '') {
+    alert("Hej, you must write something!");
+    } else {
+    let list = $('#list');
+    list.append(li);
+}
 // [2] user can cross out an item with x
 
 // [3] user can delete an item 
